a054f0edb96ca659a73dfbaaeb2463f53964bf8e
iheeresh <heereshd@gmail.com>
2023-09-29 18:24:32 +0530

    style modificstion & improvements

--- patterns/ribbon.php
+++ patterns/ribbon.php
@@ -10,19 +10,23 @@
 return array(
     'title'      =>__( 'Ribbon Section', 'vayu-x' ),
     'categories' => array( 'vayu-x' ),
-    'content'    => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","right":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
-    <div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--80);padding-right:0;padding-bottom:var(--wp--preset--spacing--80);padding-left:0"><!-- wp:heading {"textAlign":"center","textColor":"prim-color","fontSize":"wide"} -->
-    <h2 class="wp-block-heading has-text-align-center has-prim-color-color has-text-color has-wide-font-size">Love to taste new</h2>
-    <!-- /wp:heading -->
-    
-    <!-- wp:paragraph {"align":"center","textColor":"sec-color"} -->
-    <p class="has-text-align-center has-sec-color-color has-text-color">We loves our customers &amp; respects them alot.  Aenean Aenean eu mi dictum, faucibus lacus in, euismod turpis. </p>
-    <!-- /wp:paragraph -->
-    
-    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|60"}}}} -->
-    <div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--60)"><!-- wp:button {"textColor":"light-text-color","className":"is-style-outline"} -->
-    <div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-light-text-color-color has-text-color wp-element-button" href="#">Watch</a></div>
-    <!-- /wp:button --></div>
-    <!-- /wp:buttons --></div>
-    <!-- /wp:group -->',
+    'content'    => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","right":"0","left":"0"},"blockGap":"0"},"border":{"bottom":{"color":"var:preset|color|sec-color","width":"1px"}}},"backgroundColor":"accent","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-accent-background-color has-background" style="border-bottom-color:var(--wp--preset--color--sec-color);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--60);padding-right:0;padding-bottom:var(--wp--preset--spacing--60);padding-left:0"><!-- wp:cover {"dimRatio":40,"overlayColor":"accent","minHeight":54,"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;min-height:54px"><span aria-hidden="true" class="wp-block-cover__background has-accent-background-color has-background-dim-40 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}},"border":{"radius":"0px","bottom":{"style":"none","width":"0px"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide" style="border-radius:0px;border-bottom-style:none;border-bottom-width:0px;padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20)"><!-- wp:heading {"textAlign":"center","fontSize":"wide"} -->
+<h2 class="wp-block-heading has-text-align-center has-wide-font-size">Love to taste new</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">We loves our customers &amp; respects them alot.  </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--30)"><!-- wp:button {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button" href="#" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)">Watch</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->',
 );
