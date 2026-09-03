c228c05fc5df1bfd4af994a724d6dbf6ec444480
iheeresh <heereshd@gmail.com>
2026-09-03 14:21:46 +0530

    comments removed

--- theme-option/theme-option.php
+++ theme-option/theme-option.php
@@ -78,6 +78,3 @@ $obj = new Vayu_theme_option();
 
 //theme option panel
 require get_template_directory() . '/theme-option/plugin-data.php';
-
-// Below line is to include rest api function created for cutom plugin to be add in the list
-// require get_template_directory() . '/theme-option/api-function/custom-download.php';
